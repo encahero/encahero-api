b21d4265ddabfafef5dc19a65f911ec61b4ec06d
dt313 <dt313.dev21@gmail.com>
2025-08-29 21:55:44 +0900

    feat(project): init base project

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CollectionsModule } from './collections/collections.module';
+import { CardsModule } from './cards/cards.module';
+import { BattleModule } from './battle/battle.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    CategoriesModule,
+    CollectionsModule,
+    CardsModule,
+    BattleModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

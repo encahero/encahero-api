680eb2f001d6436fb52f34cbabc49dd71d6d164c
dt313 <dt313.dev21@gmail.com>
2025-09-03 18:57:55 +0900

    chore: config https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import fs from 'fs';
+import path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'private-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'public-certificate.pem')),
+  }
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(helmet());
   app.enableCors();
   app.setGlobalPrefix('api/v1');
